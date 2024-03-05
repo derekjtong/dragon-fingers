24712bebd92033873131b1c206dcf188c7de56dc
Derek <109226053+derekjtong@users.noreply.github.com>
2024-03-05 06:50:04 -0500

    Remove forced formatting

--- app/components/TypeBox.tsx
+++ app/components/TypeBox.tsx
@@ -1,13 +1,7 @@
 function TypeBox() {
-  let text = `good give eye these without move life also part even what who high much
-present mean man little to open school mean there see few these no there
-real keep which more state govern from just look at`;
+  let text = `good give eye these without move life also part even what who high much present mean man little to open school mean there see few these no there real keep which more state govern from just look at`;
 
-  return (
-    <div className="whitespace-pre-wrap text-2xl text-gray-400 font-mono">
-      {text}
-    </div>
-  );
+  return <div className="text-2xl text-gray-400 font-mono">{text}</div>;
 }
 
 export default TypeBox;
