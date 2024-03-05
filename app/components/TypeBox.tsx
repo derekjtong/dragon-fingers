5d0e5790f57b2752f396f542cdba1f3d555bc046
Derek <109226053+derekjtong@users.noreply.github.com>
2024-03-05 06:32:26 -0500

    Add type box

---
+++ app/components/TypeBox.tsx
@@ -0,0 +1,9 @@
+function TypeBox() {
+  let text = `good give eye these without move life also part even what who high much
+present mean man little to open school mean there see few these no there
+real keep which more state govern from just look at`;
+
+  return <div className="whitespace-pre-wrap text-2xl text-gray-400">{text}</div>;
+}
+
+export default TypeBox;
