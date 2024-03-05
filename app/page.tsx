2de1e7b4eeca6100983819a02f80ae485fa87683
Derek <109226053+derekjtong@users.noreply.github.com>
2024-03-05 06:49:54 -0500

    Add login button

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import TypeBox from "./components/TypeBox";
 
 export default function Home() {
   return (
-    <div className="bg-zinc-700 w-full min-h-screen flex flex-col">
-      <div className="flex justify-center pt-10">
+    <div className="bg-zinc-700 w-full min-h-screen flex flex-col px-40">
+      <div className="flex justify-between pt-10">
         <Logo />
+        <div className="text-xl text-gray-400 hover:text-gray-200 hover:cursor-pointer">
+          Login
+        </div>
       </div>
       <div className="flex flex-grow items-center justify-center">
         <TypeBox />
