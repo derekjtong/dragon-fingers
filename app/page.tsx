be8437aecccc2f2c598367c8d62fdc27de9e18fa
Derek <109226053+derekjtong@users.noreply.github.com>
2024-03-11 21:44:54 -0400

    Add stopwatch

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import { useState } from "react";
 import Logo from "./components/Logo";
+import Stopwatch from "./components/Stopwatch";
 import TypeBox from "./components/TypeBox";
 
 export default function Home() {
+  const [time, setTime] = useState<number>(0);
+  const [timerOn, setTimerOn] = useState<boolean>(false);
   return (
     <div className="bg-zinc-700 w-full min-h-screen flex flex-col px-40">
       <div className="flex justify-between pt-10">
@@ -11,7 +16,15 @@ export default function Home() {
         </div>
       </div>
       <center className="flex flex-grow items-center justify-center">
-        <TypeBox />
+        <div>
+          <Stopwatch
+            time={time}
+            setTime={setTime}
+            timerOn={timerOn}
+            setTimerOn={setTimerOn}
+          />
+          <TypeBox timerOn={timerOn} setTimerOn={setTimerOn} />
+        </div>
       </center>
     </div>
   );
